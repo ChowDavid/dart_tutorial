f3882204517343f78cbf810bae8d635a66ef5811
david <ykchowaa@gmail.com>
2020-07-29 12:49:48 +1000

    Assignment 2

---
+++ bin/assignment/prime.dart
@@ -0,0 +1,38 @@
+
+
+void main(List<String> args){
+  var minNum =2;
+  var maxNUm=20;
+  var start = DateTime.now().millisecondsSinceEpoch;
+  var primes = primeBetween(min:minNum,max:maxNUm);
+  var lastfor = DateTime.now().millisecondsSinceEpoch-start;
+  print('Numbe between $minNum and $maxNUm has ${primes.length} primes numbers, told $lastfor ms');
+  print(primes);
+}
+
+List<int> primeBetween({int min, int max}){
+  var primeNumbers = List();
+  var maxReach = max/2.ceil();
+  int product;
+  for (var i=(min<2?2:min); i<=max;i++){
+    var prime=true;
+    for (var n=2;n<=maxReach;n++){
+      for (var m=2;m<maxReach;m++) {
+        product = m*n;
+        if (product<min) break;
+        if (product>i)break;
+        if (product == i) {
+          prime = false;
+          break;
+        }
+      }
+      if (!prime){
+        break;
+      }
+    }
+    if (prime){
+      primeNumbers.add(i);
+    }
+  }
+  return primeNumbers;
+}
