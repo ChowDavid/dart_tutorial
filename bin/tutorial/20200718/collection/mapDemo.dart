494c01997d584239191c981ede97f0ee7f01f180
david <ykchowaa@gmail.com>
2020-07-29 18:44:32 +1000

    update format

--- bin/tutorial/20200718/collection/mapDemo.dart
+++ bin/tutorial/20200718/collection/mapDemo.dart
@@ -1,25 +1,22 @@
 import 'dart:collection';
 
-void main(List<String> args){
+void main(List<String> args) {
+  Map map, map2, map3;
+  map = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'c': 5};
 
-  Map map,map2,map3;
-   map = {'a':1,'b':2,'c':3,'d':4,'c':5};
+  map2 = HashMap();
+  map2['d'] = 4;
+  map2['a'] = 1;
+  map2['b'] = 2;
+  map2['c'] = 3;
+  map2['c'] = 5;
 
-   map2 =  HashMap();
-   map2['d']=4;
-   map2['a']=1;
-   map2['b']=2;
-   map2['c']=3;
-   map2['c']=5;
-
-   map3=  HashMap();
-   map3.putIfAbsent('a', () => 1);
-   map3.putIfAbsent('b', () => 2);
-   map3.putIfAbsent('c', () => 3);
-   map3.putIfAbsent('d', () => 4);
-   map3.putIfAbsent('c', () => 5);
-
-
+  map3 = HashMap();
+  map3.putIfAbsent('a', () => 1);
+  map3.putIfAbsent('b', () => 2);
+  map3.putIfAbsent('c', () => 3);
+  map3.putIfAbsent('d', () => 4);
+  map3.putIfAbsent('c', () => 5);
 
   print(map);
   print(map2);
@@ -29,6 +26,4 @@ void main(List<String> args){
   print(map.keys);
   print(map.values);
   print(map['d']);
-
-
 }
